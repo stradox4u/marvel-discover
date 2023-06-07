f73c12506f2fba46c35b97fdbfa4e013c1bde463
stradox4u <stradox@gmail.com>
2023-06-07 13:49:51 +0100

    added meta tags to the index.html

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,8 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/no-babel',
   transform: {
     '^.+\\.vue$': 'vue-jest',
-    "^.+\\.[t|j]sx?$": "babel-jest"
-    // '^.+\\js$': 'babel-jest'
+    "^.+\\.[t|j]sx?$": "babel-jest",
+    '^.+\\js$': 'babel-jest'
   },
   moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node']
 }
